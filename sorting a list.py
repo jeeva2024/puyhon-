0681ac1fc59172837c0ded502526f6a2cb589c77
jeeva2024 <hackedver@gmail.com>
2024-09-20 19:27:14 +0530

    new feats added

--- sorting a list.py
+++ sorting a list.py
@@ -1,5 +1,12 @@
 # sorting a list 
-L = [5,4,8,76,4,-5,3,-6,3,0,-11,234,876,534,1]
+ip = int(input("first enter range of list and enter the list values one by one:"))
+L = []
+for c in range(ip):
+    ele = int(input())
+    L.append(ele) 
+print("")
+print("Your List",L)
+print("")
 for a in range(len(L)):
     m = L[a]
     idx = a
@@ -12,4 +19,18 @@ for a in range(len(L)):
     tmp = L[a]
     L[a] = m
     L[idx] = tmp
-print (L)  
+print ("Sorted List in Asscending Order",L) 
+print("") 
+for c in range(len(L)):
+    l = L[c]
+    idx = c
+    count = c
+    for d in range(c,len(L)):
+        if L[d] > l:
+            l = L[d]
+            idx = count
+        count += 1
+    ntmp = L[c]
+    L[c] = l
+    L[idx] = ntmp
+print("Sorted List in Decending Order",L)
