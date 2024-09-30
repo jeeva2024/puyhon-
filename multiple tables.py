6a61a1f9c36fbd3294b66581fdedeeb07a61f4ea
jeeva2024 <hackedver@gmail.com>
2024-09-30 16:36:21 +0530

    Change:reduced loop

--- multiple tables.py
+++ multiple tables.py
@@ -2,7 +2,7 @@ a = int(input("enter the starting table:"))
 b = int(input("enter the ending table:"))
 c = int(input("enter the number of bottom:"))
 while a <= b:
-    for d in range(1,c+1):
+    for d in range(c+1):
         print(a,'x',d,'=',a*d)
     if a<b:
         print('next table')
